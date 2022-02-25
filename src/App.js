e1ca1ee93ac19c8db0e9b01ee39b7241a4edee81
Alvaro Atunez <alvaroatunez@gmail.com>
2022-02-24 20:02:13 -0500

    Componente parrafo creado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Parrafo from './components/Parrafo';
+import Variables from './components/Variables';
 
 function App() {
   return (
     <div className="App">
-
+      <Parrafo/>
+      <Variables/>      
     </div>
   );
 }
