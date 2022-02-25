534c463f0eaebd8071e20235da431a2bdacf401c
Alvaro Atunez <alvaroatunez@gmail.com>
2022-02-24 21:23:41 -0500

    Componente formulario creado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
 import Contador from './components/Contador';
 import Eventos from './components/Eventos';
+import Formulario from './components/Formulario';
 import Listas from './components/Listas';
 import Parrafo from './components/Parrafo';
 import Variables from './components/Variables';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-5">
+      {/*
       <Parrafo/>
       <Variables/>
       <Eventos/>   
       <Contador/>  
       <Listas/> 
+      */}
+      <Formulario/>
     </div>
   );
 }
